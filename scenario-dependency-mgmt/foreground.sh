2765991c3eecb77ef338eebaa56c3d6cb552bb4e
mvega-bdb <mvega@bancodebogota.com.co>
2022-04-25 10:17:16 -0500

    Add install maven and artifactory

--- scenario-dependency-mgmt/foreground.sh
+++ scenario-dependency-mgmt/foreground.sh
@@ -3,3 +3,8 @@
 sudo apt update
 sudo apt install default-jdk
 javac -version
+sudo apt install maven
+docker run --name artifactory -d -p 8081:8081 docker.bintray.io/jfrog/artifactory-cpp-ce:latest
+
+
+
