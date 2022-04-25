1084beb5534e7c42cc57c3964304531a6183bd97
mvega-bdb <mvega@bancodebogota.com.co>
2022-04-25 11:03:42 -0500

    Add install gradle and artifactory

--- scenario-dependency-mgmt/foreground.sh
+++ scenario-dependency-mgmt/foreground.sh
@@ -6,8 +6,9 @@ sudo apt -y install vim apt-transport-https dirmngr wget software-properties-com
 sudo apt install default-jdk
 javac -version
 sudo apt install maven
-sudo apt -y install gradle
 docker run --name artifactory -p 8081:8081 -d -p 8082:8082 docker.bintray.io/jfrog/artifactory-cpp-ce:latest
+sudo apt -y install gradle
+
 
 
 
