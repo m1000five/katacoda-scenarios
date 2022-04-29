0daa8932c158633e35b7300c0781adc3b7d60cf6
mvega-bdb <mvega@bancodebogota.com.co>
2022-04-29 09:25:51 -0500

    Add java libs to module core for web app

--- scenario-dependency-mgmt/foreground.sh
+++ scenario-dependency-mgmt/foreground.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
 #sudo add-apt-repository -y ppa:cwchien/gradle
-sudo apt update
+#sudo apt update
 #sudo apt install default-jdk
 #sudo apt install maven
 #sudo apt -y install gradle
+#sudo apt install openjdk-11-jdk
+
 
 
 
