dd87f1ecb7855c43eaf99d7abf19b3b728b02131
mvega-bdb <mvega@bancodebogota.com.co>
2022-04-29 08:31:36 -0500

    Change image to java

--- scenario-dependency-mgmt/foreground.sh
+++ scenario-dependency-mgmt/foreground.sh
@@ -2,8 +2,8 @@
 
 #sudo add-apt-repository -y ppa:cwchien/gradle
 sudo apt update
-sudo apt install default-jdk
-sudo apt install maven
+#sudo apt install default-jdk
+#sudo apt install maven
 #sudo apt -y install gradle
 
 
